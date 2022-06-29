baf33031df6118b5bdbd698a7d74f7c5d65a9cfb
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-06-28 22:34:31 -0300

    Adicionada ConnectionString

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using SistemaLocacao.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<SistemaLocacaoDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
