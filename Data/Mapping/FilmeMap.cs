dd28456bd0166e709a0f08a2352b82d272cbe5f1
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-06-28 22:37:04 -0300

    Adicionado mapeamento de Filme

---
+++ Data/Mapping/FilmeMap.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaLocacao.Models;
+
+namespace SistemaLocacao.Data.Mapping
+{
+    public class FilmeMap : IEntityTypeConfiguration<Filme>
+    {
+        public void Configure(EntityTypeBuilder<Filme> builder)
+        {
+            builder.ToTable("Filmes");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseMySqlIdentityColumn();
+
+            builder.Property(x => x.Titulo)
+                .IsRequired()
+                .HasColumnName("Titulo")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(100);
+
+            builder.Property(x => x.ClassificacaoIndicativa)
+                .IsRequired()
+                .HasColumnName("ClassificacaoIndicativa")
+                .HasColumnType("INT");
+
+            builder.Property(x => x.Lancamento)
+                .IsRequired()
+                .HasColumnName("Lancamento")
+                .HasColumnType("TINYINT");
+
+            builder.HasIndex(x => x.Id, "PRIMARY");
+
+            builder.HasIndex(x => x.Lancamento, "idx_Lancamento")
+                .IsUnique();
+
+            builder.HasIndex(x => x.Titulo, "idx_Titulo")
+                .IsUnique();
+        }
+    }
+}
