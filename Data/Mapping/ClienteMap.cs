bab9be7650e4580d3011f53e36256a41764be117
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-06-28 22:36:53 -0300

    Adicionado mapeamento de Cliente

---
+++ Data/Mapping/ClienteMap.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaLocacao.Models;
+
+namespace SistemaLocacao.Data.Mapping
+{
+    public class ClienteMap : IEntityTypeConfiguration<Cliente>
+    {
+        public void Configure(EntityTypeBuilder<Cliente> builder)
+        {
+            builder.ToTable("Clientes");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseMySqlIdentityColumn();
+
+            builder.Property(x => x.Nome)
+                .IsRequired()
+                .HasColumnName("Nome")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(200);
+
+            builder.Property(x => x.CPF)
+                .IsRequired()
+                .HasColumnName("CPF")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(11);
+
+            builder.HasIndex(x => x.Id, "PRIMARY");
+
+            builder.HasIndex(x => x.CPF, "idx_CPF")
+                .IsUnique();
+
+            builder.HasIndex(x => x.Nome, "idx_NOME")
+                .IsUnique();
+        }
+    }
+}
