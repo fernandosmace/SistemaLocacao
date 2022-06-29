56dfc54cdab5b7d7ecc6eaaad39d12d81b847aef
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-06-28 23:09:12 -0300

    Reorganizadas as propriedades do model Filme

--- Models/Filme.cs
+++ Models/Filme.cs
@@ -2,13 +2,12 @@ namespace SistemaLocacao.Models
 {
     public class Filme
     {
-        public IList<Locacao> Locacoes { get; set; }
         public int Id { get; set; }
         public string? Titulo { get; set; }
         public int ClassificacaoIndicativa { get; set; }
         public byte Lancamento { get; set; }
 
-
+        public IList<Locacao> Locacoes { get; set; }
 
         public Filme()
             => Locacoes = new List<Locacao>();
