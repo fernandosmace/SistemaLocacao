1387650a902fadba094fe83fa190a1f18e9a21f4
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-06-29 21:11:08 -0300

    Remover nuláveis dos DTOs de Output

--- DTO/Output/ClienteOutput.cs
+++ DTO/Output/ClienteOutput.cs
@@ -3,8 +3,8 @@ namespace SistemaLocacao.DTO.Output
     public class ClienteOutput
     {
         public int Id { get; set; }
-        public string? Nome { get; set; }
-        public string? CPF { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
         public DateTime DataNascimento { get; set; }
     }
 }
--- DTO/Output/FilmeOutput.cs
+++ DTO/Output/FilmeOutput.cs
@@ -3,7 +3,7 @@ namespace SistemaLocacao.DTO.Output
     public class Filme
     {
         public int Id { get; set; }
-        public string? Titulo { get; set; }
+        public string Titulo { get; set; }
         public int ClassificacaoIndicativa { get; set; }
         public byte Lancamento { get; set; }
     }
