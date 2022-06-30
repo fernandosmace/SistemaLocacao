1ad8dd15f110ca8295ca43c477cb6987dd4729f2
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-06-29 23:53:38 -0300

    Add máximo de caracteres no DTO Input de Cliente

--- DTO/Input/ClienteInput.cs
+++ DTO/Input/ClienteInput.cs
@@ -5,9 +5,13 @@ namespace SistemaLocacao.DTO.Input
     public class ClienteInput
     {
         [Required]
+        [MaxLength(200)]
         public string Nome { get; set; }
+
         [Required]
+        [MaxLength(11)]
         public string CPF { get; set; }
+
         [Required]
         public DateTime DataNascimento { get; set; }
     }
