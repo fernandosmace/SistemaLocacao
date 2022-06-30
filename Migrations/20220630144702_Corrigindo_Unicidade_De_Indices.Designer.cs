0079aaeac7aa470c9d374a6b541200927815ebe9
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-06-30 11:55:13 -0300

    Corrigindo unicidades nos idx de Cliente e Filme

--- Migrations/20220630144702_Corrigindo_Unicidade_De_Indices.Designer.cs
+++ Migrations/20220630144702_Corrigindo_Unicidade_De_Indices.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SistemaLocacao.Data;
 
@@ -11,9 +12,10 @@ using SistemaLocacao.Data;
 namespace SistemaLocacao.Migrations
 {
     [DbContext(typeof(SistemaLocacaoDbContext))]
-    partial class SistemaLocacaoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220630144702_Corrigindo_Unicidade_De_Indices")]
+    partial class Corrigindo_Unicidade_De_Indices
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
