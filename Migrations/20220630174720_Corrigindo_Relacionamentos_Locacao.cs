925bc447be49c4b3a011c25db71c1f3982943c03
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-06-30 15:07:15 -0300

    Migration invertendo relacionamentos de Locacao

---
+++ Migrations/20220630174720_Corrigindo_Relacionamentos_Locacao.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaLocacao.Migrations
+{
+    public partial class Corrigindo_Relacionamentos_Locacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
