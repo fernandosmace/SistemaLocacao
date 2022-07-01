036c370b643b6391de10fab312bb743b318235fe
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-07-01 16:37:53 -0300

    Add tela de listagem de Clientes

---
+++ ClientApp/src/components/Cliente/Clientes.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { DisplayBoard } from "./DisplayBoard/DisplayBoard";
+import { Button, Space } from "antd";
+
+export class Clientes extends Component {
+  render() {
+    return (
+      <Space
+        direction="vertical"
+        size="middle"
+        style={{
+          display: "flex",
+        }}
+      >
+        <div className="btnAdd">
+          <Button type="primary" href="/create-cliente">
+            Adicionar Cliente
+          </Button>
+        </div>
+        <DisplayBoard></DisplayBoard>;
+      </Space>
+    );
+  }
+}
+
+export default Clientes;
