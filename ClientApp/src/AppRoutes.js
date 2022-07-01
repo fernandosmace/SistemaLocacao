af0fa5dbde00b0506c3ec2cb71315036aa0b4c76
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-06-30 23:01:25 -0300

    Add rota para CreateCliente

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,20 +1,25 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import { CreateCliente } from "./components/Cliente/CreateCliente/CreateCliente";
 
 const AppRoutes = [
   {
     index: true,
-    element: <Home />
+    element: <Home />,
   },
   {
-    path: '/counter',
-    element: <Counter />
+    path: "/counter",
+    element: <Counter />,
   },
   {
-    path: '/fetch-data',
-    element: <FetchData />
-  }
+    path: "/fetch-data",
+    element: <FetchData />,
+  },
+  {
+    path: "/create-cliente",
+    element: <CreateCliente />,
+  },
 ];
 
 export default AppRoutes;
