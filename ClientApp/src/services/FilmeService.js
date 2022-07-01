7a24c10395b2a1cf83908400f0805919bee7f99a
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-06-30 23:36:50 -0300

    Add serviço para filme

---
+++ ClientApp/src/services/FilmeService.js
@@ -0,0 +1,40 @@
+export async function getFilme(idFilme) {
+  const response = await fetch(
+    `https://localhost:7273/api/v1/Filme/${idFilme}`
+  );
+  return await response.json();
+}
+
+export async function getAllFilmes() {
+  const response = await fetch("https://localhost:7273/api/v1/Filme");
+  return await response.json();
+}
+
+export async function createFilme(data) {
+  const response = await fetch("https://localhost:7273/api/v1/Filme", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  return await response.json();
+}
+
+export async function updateFilme(data) {
+  const response = await fetch("https://localhost:7273/api/v1/Filme", {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  return await response.json();
+}
+
+export async function deleteFilme(idFilme) {
+  const response = await fetch(
+    `https://localhost:7273/api/v1/Filme/${idFilme}`,
+    {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+  return await response.json();
+}
