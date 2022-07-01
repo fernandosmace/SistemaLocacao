3d2607128a9566cf3bb1c448f57eea1ac2172c44
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-06-30 23:32:09 -0300

    Add Serviço para Cliente

---
+++ ClientApp/src/services/ClienteService.js
@@ -0,0 +1,40 @@
+export async function getCliente(idCliente) {
+  const response = await fetch(
+    `https://localhost:7273/api/v1/Cliente/${idCliente}`
+  );
+  return await response.json();
+}
+
+export async function getAllClientes() {
+  const response = await fetch("https://localhost:7273/api/v1/Cliente");
+  return await response.json();
+}
+
+export async function createCliente(data) {
+  const response = await fetch("https://localhost:7273/api/v1/Cliente", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  return await response.json();
+}
+
+export async function updateCliente(data) {
+  const response = await fetch("https://localhost:7273/api/v1/Cliente", {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  return await response.json();
+}
+
+export async function deleteCliente(idCliente) {
+  const response = await fetch(
+    `https://localhost:7273/api/v1/Cliente/${idCliente}`,
+    {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+  return await response.json();
+}
