5fe64c5c339c72f657d6824886d38e2878f1bde1
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-07-02 10:20:10 -0300

    Add rota para tela de filmes

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -4,19 +4,12 @@ import { Home } from "./components/Home";
 import { Clientes } from "./components/Cliente/Clientes";
 import { CreateCliente } from "./components/Cliente/CreateCliente";
 import { UpdateCliente } from "./components/Cliente/UpdateCliente";
+import { Filmes } from "./components/Filme/Filmes";
 
 export const AppRoutes = [
   {
     index: true,
     element: <Home />,
-  },
-  {
-    path: "/counter",
-    element: <Counter />,
-  },
-  {
-    path: "/fetch-data",
-    element: <FetchData />,
   },
   {
     path: "/clientes",
@@ -29,6 +22,10 @@ export const AppRoutes = [
   {
     path: "/update-cliente/:id",
     element: <UpdateCliente />,
+  },
+  {
+    path: "/filmes",
+    element: <Filmes />,
   },
 ];
 
