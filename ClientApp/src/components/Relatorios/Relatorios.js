a7f3ef962dce278749b81b0691325abc39d2704b
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-07-03 15:29:58 -0300

    Aumentado tamanho ta tela de relatórios

--- ClientApp/src/components/Relatorios/Relatorios.js
+++ ClientApp/src/components/Relatorios/Relatorios.js
@@ -17,20 +17,21 @@ const Relatorios = () => {
       <Tabs
         defaultActiveKey="1"
         tabPosition="left"
+        size="large"
         style={{
           height: 500,
         }}
       >
-        <TabPane tab={"Clientes em Atraso"} key={1}>
+        <TabPane tab={"Clientes em atraso"} key={1}>
           <ClientesEmAtraso />
         </TabPane>
         <TabPane tab={"Filmes nunca alugados"} key={2}>
           <FilmesNuncaAlugados />
         </TabPane>
-        <TabPane tab={"Cinco mais alugados no último ano"} key={3}>
+        <TabPane tab={"Cinco filmes mais alugados no último ano"} key={3}>
           <FilmesMaisAlugadosAno />
         </TabPane>
-        <TabPane tab={"Três menos alugados da última semana"} key={4}>
+        <TabPane tab={"Três filmes menos alugados da última semana"} key={4}>
           <FilmesMenosAlugadosSemana />
         </TabPane>
         <TabPane tab={"Segundo maior cliente"} key={5}>
